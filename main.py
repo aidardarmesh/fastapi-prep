da869f8efaa8d961228c4c9acb51d2996306fef9
Aidar Darmesh <darmesh.aidar@gmail.com>
2024-09-09 17:09:35 +0500

    used ES to store bulk facebook user data sample

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
+from elasticsearch import Elasticsearch
 from pydantic import BaseModel
+
 
 app = FastAPI()
 
+es = Elasticsearch("http://localhost:9200")
+# phone, id, first_name, last_name, gender
+FB_PROFILE_URL = f"https://www.facebook.com/profile.php?id={id}"
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool | None = None
+
+class SearchQuery(BaseModel):
+    query: str
 
 
 @app.get("/")
@@ -15,11 +19,15 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_id": item_id, "item_name": item.name}
+@app.post("/search")
+async def search(q: SearchQuery):
+    return es.search(
+        index="first_index",
+        body={
+            "query": {
+                "match": {
+                    "phone_number": q.query
+                }
+            }
+        }
+    )
