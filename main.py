11f97ba4be2efbef627ca696938fa89e1b5eb3a6
Aidar Darmesh <darmesh.aidar@gmail.com>
2024-09-09 21:15:00 +0500

    added TODOs that will fix later

--- main.py
+++ main.py
@@ -5,13 +5,24 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-es = Elasticsearch("http://localhost:9200")
+es = Elasticsearch(host="es")
 # phone, id, first_name, last_name, gender
 FB_PROFILE_URL = f"https://www.facebook.com/profile.php?id={id}"
+FB_DATA_DIR = "./data/fb.txt"
 
 
 class SearchQuery(BaseModel):
     query: str
+
+
+@app.on_event("startup")
+async def load_bulk_data():
+    # create es index
+    with open(FB_DATA_DIR, 'r') as file:
+        for line in file:
+            bulk_data = line.split(':')
+            # put bulk data
+            # try to use batch processing
 
 
 @app.get("/")
